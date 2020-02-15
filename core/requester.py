868a8fb2739959ef6a72d89f605f5849c1ea732a
Ocraz <registered@quicknet.nl>
2020-02-15 13:34:41 +0100

    Add files via upload
    Add extra error handling

--- core/requester.py
+++ core/requester.py
@@ -4,7 +4,7 @@ import time
 import random
 import warnings
 import requests
-
+import sys
 import core.config
 
 warnings.filterwarnings('ignore') # Disable SSL related warnings
@@ -16,8 +16,13 @@ def requester(url, data, headers, GET, delay):
         delay = random.choice(range(6, 12))
     time.sleep(delay)
     headers['Host'] = re.search(r'https?://([^/]+)', url).group(1)
+    # Added try/except (ZZ)
     if GET:
-        response = requests.get(url, params=data, headers=headers, verify=False)
+        try:
+            response = requests.get(url, params=data, headers=headers, verify=False)
+        except:
+            sys.stderr.write('ERROR: %sn' % str(err))
+            response = False 
     elif core.config.globalVariables['jsonData']:
         response = requests.post(url, json=data, headers=headers, verify=False)
     else:
